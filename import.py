61f5b3284757aac646c9ade0a1f1bf9086dcd7ab
Nithya Rajan <nithyar2005@gmail.com>
2018-04-30 15:48:26 -0700

    Add files via upload

---
+++ import.py
@@ -0,0 +1,25 @@
+import sys, os, django
+
+
+sys.path.append("/Users/nithyarajan/Cmpe172/fatcat/") #here store is root folder(means parent).
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fatcat.settings")
+django.setup()
+
+from home.models import Employee
+import csv
+
+with open('Employees.csv') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        # The header row values become your keys
+        name = row['NAME'].strip()
+        job = row['JOB TITLE'].strip()
+        department = row['DEPARTMENT'].strip()
+        salary = row['EMPLOYEE ANNUAL SALARY'].strip()
+        salary2 = row['ESTIMATED ANNUAL SALARY MINUS FURLOUGHS'].strip()
+
+        new_employee = Employee(name = name, job = job, department = department, salary = salary, salary2 = salary2)
+        print(new_employee.name)
+        print(new_employee.job)
+        new_employee.save()
