2cc93b82857001cf9fb8b912b1ee902102adbb1d
Nithya Rajan <nithyar2005@gmail.com>
2018-04-30 16:26:18 -0700

    Update views.py with results method

--- home/views.py
+++ home/views.py
@@ -3,6 +3,10 @@ from django.contrib.auth.decorators import login_required
 
 import json
 
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Employee
+from django.http import Http404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def index(request):
     return render(request, 'index.html')
@@ -25,3 +29,22 @@ def dashboard(request):
 
 def deptsearch(request):
     return render(request, 'deptsearch.html')
+
+def results(request):
+    employee_list = Employee.objects.all()
+    paginator = Paginator(employee_list, 52)  # Show 52 employees per page
+
+    page = request.GET.get('page')
+    try:
+        employees = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        employees = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        employees = paginator.page(paginator.num_pages)
+
+    context = {
+        'employees': employees,
+    }
+    return render(request, 'results.html', context)
