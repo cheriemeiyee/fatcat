989f91f17c5b5a5032d09db875dcfa2c6c8ddc26
Nithya Rajan <nithyar2005@gmail.com>
2018-04-30 15:49:26 -0700

    Update models.py

--- home/models.py
+++ home/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Employee(models.Model):
+    name = models.CharField(max_length=300)
+    job = models.CharField(max_length=300)
+    department = models.CharField(max_length=300)
+    salary = models.CharField(max_length=300)
+    salary2 = models.CharField(max_length=300)
+
+
+    def __str__(self):
+        return self.name
